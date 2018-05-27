68980ecaf5a256863c58351e4b9825101cb647fd
Miclefeng <miclefengzss@gmail.com>
2018-05-27 10:59:56 +0800

    edit

--- app/secure.py
+++
@@ -1,9 +0,0 @@
-###############################################################
-# File Name: secure.py
-# Author: miclefeng
-# mail: miclefengzss@163.com
-# Created Time: Wed 23 May 2018 10:08:32 PM CST
-#=============================================================
-# coding:utf8
-
-DEBUG = True
