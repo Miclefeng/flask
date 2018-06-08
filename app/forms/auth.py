d204e04249c9fd796381797a472a65c0521daacf
ZSShang <1280054628@qq.com>
2018-06-09 00:10:22 +0800

    newchangelist

--- app/forms/auth.py
+++ app/forms/auth.py
@@ -10,11 +10,13 @@ from wtforms.validators import DataRequired, Length, Email, ValidationError
 from app.models.user import User
 
 
-class RegisterForm(Form):
+class LoginForm(Form):
     email = StringField(validators=[DataRequired(), Length(8, 64), Email(message='电子邮箱不符合规范')])
 
     password = PasswordField(validators=[DataRequired(message='密码不能为空'), Length(6, 24)])
 
+
+class RegisterForm(LoginForm):
     nickname = StringField(validators=[DataRequired(), Length(2, 12, message='昵称至少为2个字符，至多12个字符')])
 
     def validate_email(self, field):
