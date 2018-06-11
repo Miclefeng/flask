b481525872801f889fe25f8599d06b109a682805
ZSShang <1280054628@qq.com>
2018-06-12 00:31:59 +0800

    edit

--- app/web/wish.py
+++ app/web/wish.py
@@ -1,7 +1,8 @@
 from . import web
-from flask import current_app, redirect, url_for, flash
+from flask import redirect, url_for, flash
 from app.models.base import db
 from app.models.wish import Wish
+from flask_login import login_required, current_user
 
 __author__ = '七月'
 
@@ -14,11 +15,11 @@ def my_wish():
 @web.route('/wish/book/<isbn>')
 @login_required
 def save_to_wish(isbn):
-    if current_app.can_save_to_list(isbn):
+    if current_user.can_save_to_list(isbn):
     	with db.auto_commit():
     		wish = Wish()
     		wish.isbn = isbn
-    		wish.uid = current_app.id
+    		wish.uid = current_user.id
     		db.session.add(wish)
     else:
     	flash('请不要重复添加')
