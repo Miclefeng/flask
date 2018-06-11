c5d6fdb70f14f4bbd6594eeff4e8e7d532a8624e
ZSShang <1280054628@qq.com>
2018-06-12 00:30:00 +0800

    edit

--- app/web/wish.py
+++ app/web/wish.py
@@ -1,4 +1,7 @@
 from . import web
+from flask import current_app, redirect, url_for, flash
+from app.models.base import db
+from app.models.wish import Wish
 
 __author__ = '七月'
 
@@ -9,8 +12,17 @@ def my_wish():
 
 
 @web.route('/wish/book/<isbn>')
+@login_required
 def save_to_wish(isbn):
-    pass
+    if current_app.can_save_to_list(isbn):
+    	with db.auto_commit():
+    		wish = Wish()
+    		wish.isbn = isbn
+    		wish.uid = current_app.id
+    		db.session.add(wish)
+    else:
+    	flash('请不要重复添加')
+    return redirect(url_for('web.book_detail', isbn=isbn))
 
 
 @web.route('/satisfy/wish/<int:wid>')
