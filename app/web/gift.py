c15d5fc36ba68df058ddeff6d5bf47d14019698d
ZSShang <1280054628@qq.com>
2018-06-13 01:22:27 +0800

    newchangelist

--- app/web/gift.py
+++ app/web/gift.py
@@ -1,6 +1,7 @@
-from flask import current_app, redirect, flash, url_for
+from flask import current_app, redirect, flash, url_for, render_template
 from app.models.base import db
 from app.models.gift import Gift
+from app.view_models.gift import MyGifts
 from . import web
 from flask_login import login_required, current_user
 
@@ -12,7 +13,8 @@ def my_gifts():
     gifts_of_mine = Gift.get_user_gifts(uid)
     isbn_list = [gift.isbn for gift in gifts_of_mine]
     wish_count_list = Gift.get_wish_counts(isbn_list)
-    return 'My gifts'
+    view_model = MyGifts(gifts_of_mine, wish_count_list)
+    return render_template('my_gifts.html', gifts=view_model.gifts)
 
 
 @web.route('/gifts/book/<isbn>')
