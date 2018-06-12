6bbe175af78a7767f22e29786d249b7677e7186b
ZSShang <1280054628@qq.com>
2018-06-13 00:45:25 +0800

    newchangelist

--- app/models/gift.py
+++ app/models/gift.py
@@ -5,9 +5,10 @@
 # Created Time: 2018/6/7 23:07
 #=============================================================
 # coding:utf8
-from sqlalchemy import Column, Integer, ForeignKey, Boolean, String, desc
+from sqlalchemy import Column, Integer, ForeignKey, Boolean, String, desc, func
 from sqlalchemy.orm import relationship
-from app.models.base import Base
+from app.models.base import Base, db
+from app.models.wish import Wish
 from flask import current_app
 from app.spider.fisher_book import FisherBook
 
@@ -26,6 +27,16 @@ class Gift(Base):
         fisher_book = FisherBook()
         fisher_book.search_by_isbn(self.isbn)
         return fisher_book.first
+
+    @classmethod
+    def get_user_gifts(cls, uid):
+        gifts = Gift.query.filter_by(uid=uid, launched=False).order_by(desc(Gift.create_time)).all()
+        return gifts
+
+    @classmethod
+    def get_wish_counts(cls, isbn_list):
+        count_list = db.session.query(func.count(Wish.id), Wish.isbn).filter(Wish.launched == False, Wish.isbn.in_(isbn_list), Wish.status == 1).group_by(Wish.isbn).all()
+        return [{'count' : w[0], 'isbn': w[1]} for w in count_list]
 
     # 对象代表一个礼物，具体的事物
     # 类代表这个事物，是抽象的，不是具体的“一个”
