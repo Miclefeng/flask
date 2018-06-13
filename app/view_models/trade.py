1f9396a1343feaf865a45c6ef754892449275b1a
ZSShang <1280054628@qq.com>
2018-06-13 21:56:58 +0800

    newchangelist

--- app/view_models/trade.py
+++ app/view_models/trade.py
@@ -1,3 +1,4 @@
+from app.view_models.book import BookViewModel
 
 
 class TradeInfo:
@@ -19,3 +20,30 @@ class TradeInfo:
 			user_name=single.user.nickname,
 			time=time,
 			id=single.id)
+
+
+class MyTrades:
+	def __init__(self, trades_of_mine, trade_count_list):
+		self.__trades_of_mine = trades_of_mine
+		self.__trade_count_list = trade_count_list
+		self.trades = self.__parse()
+
+	def __parse(self):
+		temp_trades = []
+		for trade in self.__trades_of_mine:
+			temp_trades.append(self.__matching(trade))
+		return temp_trades
+
+	def __matching(self, trade):
+		count = 0
+		for trade_count in self.__trade_count_list:
+			if trade_count['isbn'] == trade.isbn:
+				count = trade_count['count']
+				break
+
+		r = {
+			'wishes_count': count,
+			'book': BookViewModel(trade.book),
+			'id': trade.id
+        }
+        return r
--- app/web/wish.py
+++ app/web/wish.py
@@ -1,9 +1,9 @@
+from app.view_models.trade import MyTrades
 from . import web
 from flask import redirect, url_for, flash, render_template
 from app.models.base import db
 from app.models.wish import Wish
 from flask_login import login_required, current_user
-from app.view_models.wish import MyWishes
 
 
 @web.route('/my/wish')
@@ -13,8 +13,8 @@ def my_wish():
     wishes_of_mine = Wish.get_user_wishes(uid)
     isbn_list = [wish.isbn for wish in wishes_of_mine]
     gift_count_list = Wish.get_gift_counts(isbn_list)
-    view_model = MyWishes(wishes_of_mine, gift_count_list)
-    return render_template('my_wish.html', wishes=view_model.wishes)
+    view_model = MyTrades(wishes_of_mine, gift_count_list)
+    return render_template('my_wish.html', wishes=view_model.trades)
 
 @web.route('/wish/book/<isbn>')
 @login_required
